4b9e6d18ac6c34cff17b09de427ec476cee64c90
Antonio J Mattos <antonio.mattos@ifsp.edu.br>
2021-08-18 18:06:52 -0300

    fixup! Simplificar a definição de configurações

--- src/Susep.SISRH.WebApi/Program.cs
+++ src/Susep.SISRH.WebApi/Program.cs
@@ -16,16 +16,6 @@ namespace Susep.SISRH.WebApi
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    var environmentName = hostingContext.HostingEnvironment.EnvironmentName;
-                    config
-                        .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "Settings"))
-                        .AddJsonFile($"connectionStrings.{environmentName}.json", true, true)
-                        .AddJsonFile($"appsettings.{environmentName}.json", true, true)
-                        .AddJsonFile($"messagebroker.{environmentName}.json", true, true)
-                        .AddEnvironmentVariables();
-                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
